addf6a31c7ee030c7f6dedaa84d97e74272c5e93
AkshayPoriya <akshayporiya1234@gmail.com>
2020-11-08 02:27:30 +0530

    [Akshay] Find Cheapest Best Rated Hotels For Reward Customers

---
+++ HotelReservationSystem/InvalidDateTimeException.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InvalidDateTimeException.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Akshay Poriya"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace HotelReservationSystem
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class InvalidDateTimeException:Exception
+    {
+        public InvalidDateTimeException(string message) : base(message)
+        {
+            
+        }
+    }
+}
