0ebb02b4b410e89e01f401462a129b17406ecf76
AkshayPoriya <akshayporiya1234@gmail.com>
2020-11-08 01:26:00 +0530

    [Akshay] Add Reward Customers as a part of loyalty program

--- HotelReservationSystem/HotelRepository.cs
+++ HotelReservationSystem/HotelRepository.cs
@@ -16,17 +16,27 @@ namespace HotelReservationSystem
     {
         /// <summary>
         /// Each key represents hotel name and value stores a container referencing prices for each dayType
-        /// The name to price mapper regulr customer
+        /// The name to price mapper for regular customers
         /// </summary>
-        public Dictionary<string, Dictionary<DayType, double>> nameToPriceMapperRegulrCustomer;
+        public Dictionary<string, Dictionary<DayType, double>> nameToPriceMapperRegularCustomer;
 
+        /// <summary>
+        /// The name to price mapper for reward customers
+        /// </summary>
+        public Dictionary<string, Dictionary<DayType, double>> nameToPriceMapperRewardCustomer;
+
+        /// <summary>
+        /// The name to rating mapper
+        /// </summary>
         public Dictionary<string, int> nameToRatingMapper;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="HotelRepository"/> class.
         /// </summary>
         public HotelRepository()
         {
-            this.nameToPriceMapperRegulrCustomer = new Dictionary<string, Dictionary<DayType, double>>();
+            this.nameToPriceMapperRegularCustomer = new Dictionary<string, Dictionary<DayType, double>>();
+            this.nameToPriceMapperRewardCustomer = new Dictionary<string, Dictionary<DayType, double>>();
             this.nameToRatingMapper = new Dictionary<string, int>();
         }
 
@@ -37,7 +47,7 @@ namespace HotelReservationSystem
         /// <param name="startDate">The start date.</param>
         /// <param name="endDate">The end date.</param>
         /// <returns></returns>
-        public List<Tuple<string, int, double>> GetCheapestBestRatedHotels(string startDate, string endDate)
+        public List<Tuple<string, int, double>> GetCheapestBestRatedHotels(string startDate, string endDate, CustomerType customerType)
         {
             DateTime startDateTime = Convert.ToDateTime(DateTime.ParseExact(startDate, "dd/MM/yyyy", CultureInfo.InvariantCulture));
             DateTime endDateTime = Convert.ToDateTime(DateTime.ParseExact(endDate, "dd/MM/yyyy", CultureInfo.InvariantCulture));
@@ -48,7 +58,16 @@ namespace HotelReservationSystem
             int weekEnds = GetNumberOfWeekEndsBetweenTwoDates(startDateTime, endDateTime);
             int weekDays = (endDateTime - startDateTime).Days - weekEnds + 1;
             Dictionary<string, double> priceList = new Dictionary<string, double>();
-            foreach (KeyValuePair<string, Dictionary<DayType, double>> pair in this.nameToPriceMapperRegulrCustomer)
+            Dictionary<string, Dictionary<DayType, double>> nameToPriceMapper = new Dictionary<string, Dictionary<DayType, double>>();
+            if(customerType == CustomerType.Regular)
+            {
+                nameToPriceMapper = this.nameToPriceMapperRegularCustomer;
+            }
+            else
+            {
+                nameToPriceMapper = this.nameToPriceMapperRewardCustomer;
+            }
+            foreach (KeyValuePair<string, Dictionary<DayType, double>> pair in nameToPriceMapper)
             {
                 double priceForWeekDays = pair.Value[DayType.Weekday] * weekDays;
                 double priceForWeekEnds = pair.Value[DayType.Weekend] * weekEnds;
@@ -81,7 +100,7 @@ namespace HotelReservationSystem
         /// <param name="startDate">The start date.</param>
         /// <param name="endDate">The end date.</param>
         /// <returns></returns>
-        public List<Tuple<string, int, double>> GetBestRatedHotels(string startDate, string endDate)
+        public List<Tuple<string, int, double>> GetBestRatedHotels(string startDate, string endDate, CustomerType customerType)
         {
             DateTime startDateTime = Convert.ToDateTime(DateTime.ParseExact(startDate, "dd/MM/yyyy", CultureInfo.InvariantCulture));
             DateTime endDateTime = Convert.ToDateTime(DateTime.ParseExact(endDate, "dd/MM/yyyy", CultureInfo.InvariantCulture));
@@ -92,7 +111,16 @@ namespace HotelReservationSystem
             int weekEnds = GetNumberOfWeekEndsBetweenTwoDates(startDateTime, endDateTime);
             int weekDays = (endDateTime - startDateTime).Days - weekEnds + 1;
             Dictionary<string, double> priceList = new Dictionary<string, double>();
-            foreach (KeyValuePair<string, Dictionary<DayType, double>> pair in this.nameToPriceMapperRegulrCustomer)
+            Dictionary<string, Dictionary<DayType, double>> nameToPriceMapper = new Dictionary<string, Dictionary<DayType, double>>();
+            if (customerType == CustomerType.Regular)
+            {
+                nameToPriceMapper = this.nameToPriceMapperRegularCustomer;
+            }
+            else
+            {
+                nameToPriceMapper = this.nameToPriceMapperRewardCustomer;
+            }
+            foreach (KeyValuePair<string, Dictionary<DayType, double>> pair in nameToPriceMapper)
             {
                 double priceForWeekDays = pair.Value[DayType.Weekday] * weekDays;
                 double priceForWeekEnds = pair.Value[DayType.Weekend] * weekEnds;
@@ -139,5 +167,11 @@ namespace HotelReservationSystem
     {
         Weekday,
         Weekend
+    }
+
+    public enum CustomerType
+    {
+        Regular,
+        Reward
     }
 }
--- HotelReservationSystemMSTest/TestHotelRepository.cs
+++ HotelReservationSystemMSTest/TestHotelRepository.cs
@@ -24,14 +24,36 @@ namespace HotelReservationSystemMSTest
         public void AddHotelWithNameAndRateForRegularCustomer()
         {
             // Arrange
-            HotelRepository hotelRepository = PopulateHotelRepository();
+            HotelRepository hotelRepository = PopulateHotelRepositoryForRegularCustomer();
             // Act
-            double actual1 = hotelRepository.nameToPriceMapperRegulrCustomer["Lakewood"][DayType.Weekday];
+            double actual1 = hotelRepository.nameToPriceMapperRegularCustomer["Lakewood"][DayType.Weekday];
             double expected1 = 110;
-            double actual2 = hotelRepository.nameToPriceMapperRegulrCustomer["Bridgewood"][DayType.Weekend];
+            double actual2 = hotelRepository.nameToPriceMapperRegularCustomer["Bridgewood"][DayType.Weekend];
             double expected2 = 50;
-            double actual3 = hotelRepository.nameToPriceMapperRegulrCustomer["Ridgewood"][DayType.Weekend];
+            double actual3 = hotelRepository.nameToPriceMapperRegularCustomer["Ridgewood"][DayType.Weekend];
             double expected3 = 150;
+            // Assert
+            Assert.AreEqual(expected1, actual1);
+            Assert.AreEqual(expected2, actual2);
+            Assert.AreEqual(expected3, actual3);
+        }
+
+        /// <summary>
+        /// UC8
+        /// Adds the hotel with name and rate for reward customer.
+        /// </summary>
+        [TestMethod]
+        public void AddHotelWithNameAndRateForRewardCustomer()
+        {
+            // Arrange
+            HotelRepository hotelRepository = PopulateHotelRepositoryForRewardCustomer();
+            // Act
+            double actual1 = hotelRepository.nameToPriceMapperRewardCustomer["Lakewood"][DayType.Weekday];
+            double expected1 = 80;
+            double actual2 = hotelRepository.nameToPriceMapperRewardCustomer["Bridgewood"][DayType.Weekend];
+            double expected2 = 50;
+            double actual3 = hotelRepository.nameToPriceMapperRewardCustomer["Ridgewood"][DayType.Weekend];
+            double expected3 = 40;
             // Assert
             Assert.AreEqual(expected1, actual1);
             Assert.AreEqual(expected2, actual2);
@@ -46,7 +68,7 @@ namespace HotelReservationSystemMSTest
         public void AddRatingToHotels()
         {
             // Arrange
-            HotelRepository hotelRepository = PopulateHotelRepository();
+            HotelRepository hotelRepository = PopulateHotelRepositoryForRegularCustomer();
             // Act
             int actual1 = hotelRepository.nameToRatingMapper["Lakewood"];
             int expected1 = 3;
@@ -66,14 +88,14 @@ namespace HotelReservationSystemMSTest
         /// Assert Compare two list objects reference wise, thats why list is converted to string first
         /// </summary>
         [TestMethod]
-        public void ReturnCheapestHotelWithBestRatingForGivenDateRange()
+        public void ReturnCheapestHotelWithBestRatingForGivenDateRangeForRegularCustomers()
         {
             // Arrange
-            HotelRepository hotelRepository = PopulateHotelRepository();
+            HotelRepository hotelRepository = PopulateHotelRepositoryForRegularCustomer();
             // Act
             string startDate = "11/09/2020";
             string endDate = "12/09/2020";
-            List<Tuple<string, int, double>> actualListOfNameAndPrice = hotelRepository.GetCheapestBestRatedHotels(startDate, endDate);
+            List<Tuple<string, int, double>> actualListOfNameAndPrice = hotelRepository.GetCheapestBestRatedHotels(startDate, endDate, CustomerType.Regular);
             List<Tuple<string, int, double>> expectedListOfNameAndPrice = new List<Tuple<string, int, double>>();
             expectedListOfNameAndPrice.Add(new Tuple<string, int, double>("Bridgewood", 4, 200));
             string actual = ListOfTupleToString(actualListOfNameAndPrice);
@@ -87,14 +109,14 @@ namespace HotelReservationSystemMSTest
         /// Returns the hotels with best rating for given date range.
         /// </summary>
         [TestMethod]
-        public void ReturnHotelsWithBestRatingForGivenDateRange()
+        public void ReturnHotelsWithBestRatingForGivenDateRangeForRegularCustomers()
         {
             // Arrange
-            HotelRepository hotelRepository = PopulateHotelRepository();
+            HotelRepository hotelRepository = PopulateHotelRepositoryForRegularCustomer();
             // Act
             string startDate = "11/09/2020";
             string endDate = "12/09/2020";
-            List<Tuple<string, int, double>> actualListOfNameAndPrice = hotelRepository.GetBestRatedHotels(startDate, endDate);
+            List<Tuple<string, int, double>> actualListOfNameAndPrice = hotelRepository.GetBestRatedHotels(startDate, endDate, CustomerType.Regular);
             List<Tuple<string, int, double>> expectedListOfNameAndPrice = new List<Tuple<string, int, double>>();
             expectedListOfNameAndPrice.Add(new Tuple<string, int, double>("Ridgewood", 5, 370));
             string actual = ListOfTupleToString(actualListOfNameAndPrice);
@@ -103,7 +125,7 @@ namespace HotelReservationSystemMSTest
             Assert.AreEqual(expected, actual);
         }
 
-        private HotelRepository PopulateHotelRepository()
+        private HotelRepository PopulateHotelRepositoryForRegularCustomer()
         {
             HotelRepository hotelRepository = new HotelRepository();
             hotelRepository.nameToRatingMapper.Add("Lakewood", 3);
@@ -112,15 +134,36 @@ namespace HotelReservationSystemMSTest
             Dictionary<DayType, double> lakeWood = new Dictionary<DayType, double>();
             lakeWood.Add(DayType.Weekday, 110);
             lakeWood.Add(DayType.Weekend, 90);
-            hotelRepository.nameToPriceMapperRegulrCustomer.Add("Lakewood", lakeWood);
+            hotelRepository.nameToPriceMapperRegularCustomer.Add("Lakewood", lakeWood);
             Dictionary<DayType, double> bridgeWood = new Dictionary<DayType, double>();
             bridgeWood.Add(DayType.Weekday, 150);
             bridgeWood.Add(DayType.Weekend, 50);
-            hotelRepository.nameToPriceMapperRegulrCustomer.Add("Bridgewood", bridgeWood);
+            hotelRepository.nameToPriceMapperRegularCustomer.Add("Bridgewood", bridgeWood);
             Dictionary<DayType, double> ridgeWood = new Dictionary<DayType, double>();
             ridgeWood.Add(DayType.Weekday, 220);
             ridgeWood.Add(DayType.Weekend, 150);
-            hotelRepository.nameToPriceMapperRegulrCustomer.Add("Ridgewood", ridgeWood);
+            hotelRepository.nameToPriceMapperRegularCustomer.Add("Ridgewood", ridgeWood);
+            return hotelRepository;
+        }
+
+        private HotelRepository PopulateHotelRepositoryForRewardCustomer()
+        {
+            HotelRepository hotelRepository = new HotelRepository();
+            hotelRepository.nameToRatingMapper.Add("Lakewood", 3);
+            hotelRepository.nameToRatingMapper.Add("Bridgewood", 4);
+            hotelRepository.nameToRatingMapper.Add("Ridgewood", 5);
+            Dictionary<DayType, double> lakeWood = new Dictionary<DayType, double>();
+            lakeWood.Add(DayType.Weekday, 80);
+            lakeWood.Add(DayType.Weekend, 80);
+            hotelRepository.nameToPriceMapperRewardCustomer.Add("Lakewood", lakeWood);
+            Dictionary<DayType, double> bridgeWood = new Dictionary<DayType, double>();
+            bridgeWood.Add(DayType.Weekday, 110);
+            bridgeWood.Add(DayType.Weekend, 50);
+            hotelRepository.nameToPriceMapperRewardCustomer.Add("Bridgewood", bridgeWood);
+            Dictionary<DayType, double> ridgeWood = new Dictionary<DayType, double>();
+            ridgeWood.Add(DayType.Weekday, 100);
+            ridgeWood.Add(DayType.Weekend, 40);
+            hotelRepository.nameToPriceMapperRewardCustomer.Add("Ridgewood", ridgeWood);
             return hotelRepository;
         }
 
