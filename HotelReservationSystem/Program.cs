c16f2616b4b288ae272c3a2405c0586cfab47d40
AkshayPoriya <akshayporiya1234@gmail.com>
2020-11-07 21:02:38 +0530

    [Akshay] initial commit

---
+++ HotelReservationSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Akshay Poriya"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace HotelReservationSystem
+{
+    using System;
+
+    /// <summary>
+    /// Program Class Contains Main Function, entry point for the application
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hotel Reservation Program!");
+        }
+    }
+}
